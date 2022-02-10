4691fb0d0245dfb8bff94a68cdea4a3b04f4838f
Pranav <pb719@ic.ac.uk>
2022-02-10 22:38:14 +0000

    REFACTOR abstract code into inner functions

--- src/App.tsx
+++ src/App.tsx
@@ -17,36 +17,48 @@ function App() {
   })
 
 
+  function updateCell(i: number, j: number): React.MouseEventHandler<HTMLDivElement> | undefined {
+    return () => {
+      const newGrid = produce(grid, gridCopy => {
+        gridCopy[i][j] = !grid[i][j];
+        return gridCopy;
+      });
+      setGrid(newGrid);
+    };
+  }
+
+  function mapGrid(): React.ReactNode {
+    return grid.map((rows, i) => rows.map((col, j) => {
+      return <div
+        key={`${i}-${j}`}
+        style={{
+          width: 20,
+          height: 20,
+          backgroundColor: grid[i][j] ? '#6ee7b7' : undefined,
+          border: 'solid 1px black'
+        }}
+        onClick={updateCell(i, j)} />;
+    }
+    ));
+  }
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${numCols}, 20px)`
-      }}>
-      {
-        grid.map((rows, i) =>
-          rows.map((col, j) => {
-            return <div
-              key={`${i}-${j}`}
-              style={{
-                width: 20,
-                height: 20,
-                backgroundColor: grid[i][j] ? '#6ee7b7' : undefined,
-                border: 'solid 1px black'
-              }}
-              onClick={() => {
-                const newGrid = produce(grid, gridCopy => {
-                  gridCopy[i][j] = !grid[i][j];
-                  return gridCopy
-                })
-                setGrid(newGrid)
-              }}
-            />
-          }
-          ))
-      }
-    </div >
+    <>
+      <button>start</button>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}>
+        {
+          mapGrid()
+        }
+      </div>
+    </>
   );
+
+
+
+
 }
 
 export default App;
