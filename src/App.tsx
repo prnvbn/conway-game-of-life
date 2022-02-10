5c99cd2ae0f3c7add299e55c449a6e480ff98ed0
Pranav <pb719@ic.ac.uk>
2022-02-10 22:15:38 +0000

    ADD basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const numRows = 50
+const numCols = 50
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const grid = [];
+
+    for (let i = 0; i < numRows; i++) {
+      grid.push(new Array(numCols).fill(0));
+    }
+
+    return grid
+  })
+
+
   return (
-    <div className="App">
-      yo
-    </div>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}>
+      {
+        grid.map((rows, i) =>
+          rows.map((col, j) => {
+            return <div
+              key={`${i}-${j}`}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][j] ? 'red' : undefined,
+                border: 'solid 1px black'
+              }}
+            />
+          }
+          ))
+      }
+    </div >
   );
 }
 
