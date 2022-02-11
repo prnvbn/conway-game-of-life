85d0f0472beb2e324ed2ef6aa4557d9b99de989d
Pranav <pb719@ic.ac.uk>
2022-02-11 03:10:55 +0000

    ADD border buttons

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ function App() {
 
   return (
     <>
-      <div className='grid grid-cols-3 p-5 justify-items-center'>
+      <div className='grid grid-cols-3 p-5 m-5 justify-items-center border border-black'>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => {
